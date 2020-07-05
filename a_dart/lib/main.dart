da003c33c71d799fef47b397f4cf27cfeb9538f9
meyash <yashsn2127@gmail.com>
2020-07-05 12:25:16 +0530

    flut

--- a_dart/lib/main.dart
+++ a_dart/lib/main.dart
@@ -6,7 +6,16 @@ import 'package:flutter/material.dart';
 // OR
 void main() => runApp(MyApp());
 
+// every fn for a widget must go in same class 
 class MyApp extends StatelessWidget {
+
+  var quesIndex =0;
+
+  void annQues(){
+    quesIndex+=1;
+  }
+
+  var questions = ['What\'s', 'Your', 'Fav', 'Color'];
   // decorator to override stateless method
   @override
   Widget build(BuildContext context) {
@@ -17,7 +26,30 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Dart_Basics'),
         ),
-        body: Text('Hello_World'),
+        // body: Text('Hello_World'),
+        // Column : Align top to bottom
+        body: Column(
+          children: [
+            // text is a stateless widget
+            Text('The Question!'),
+            RaisedButton(
+              child: Text(questions[quesIndex]),
+              // dont execute here as only execute when user presses
+              onPressed: annQues,
+            ),
+            RaisedButton(
+              child: Text(questions.elementAt(1)),
+              onPressed: ()=> print('method_2'),
+            ),
+            RaisedButton(
+              child: Text(questions[2]),
+              onPressed: (){
+                //..
+                print('method_3');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
