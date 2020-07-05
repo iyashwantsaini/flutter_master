3433e79ddb6243e953bc3d0d17079e65d220ca8d
meyash <yashsn2127@gmail.com>
2020-07-05 11:53:48 +0530

    flut

--- a_dart/lib/main.dart
+++ a_dart/lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+// void main(){
+//   runApp(MyApp());
+// }
+// OR
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context){
+  // decorator to override stateless method
+  @override
+  Widget build(BuildContext context) {
     // material app provided by material.dart
-    return MaterialApp(home: Text('Hello_World!'),);
+    // Scaffold gives base structure of app
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dart_Basics'),
+        ),
+        body: Text('Hello_World'),
+      ),
+    );
   }
 }
