b7010ae6e7a546674e7879ffe3454abb718ce005
meyash <yashsn2127@gmail.com>
2020-07-05 11:40:57 +0530

    flut

---
+++ a_dart/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context){
+    // material app provided by material.dart
+    return MaterialApp(home: Text('Hello_World!'),);
+  }
+}
